99e9c9ed2f3730fbe694a7c17cbc10cf1f75237a
Adam Stajer <stajeradam757@gmail.com>
2023-12-09 17:47:16 +0100

    Last lesson about useEffect

---
+++ react-hooks-3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Effect1 from './components/Effect1';
+
+function App() {
+  return (
+    <div className="App">
+      <Effect1/>
+    </div>
+  );
+}
+
+export default App;
