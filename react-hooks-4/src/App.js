567711b0b51bfab23990602c034ea638019dd0de
Adam Stajer <stajeradam757@gmail.com>
2023-12-12 18:02:31 +0100

    useReducer Tutoral v2

--- react-hooks-4/src/App.js
+++ react-hooks-4/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <>
-      <CounterOne/>
+      {/* <CounterOne/> */}
+      <CounterTwo/>
     </>
   );
 }
