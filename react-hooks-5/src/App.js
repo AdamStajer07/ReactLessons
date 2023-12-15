f6669f36fbd9c4d4b7f7fa5152b7f6e268eddd8d
AdamStajer07 <stajeradam9@gmail.com>
2023-12-15 11:44:33 +0100

    Last lesson about useReducer

---
+++ react-hooks-5/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
+
+function App() {
+  return (
+    <>
+      {/* <DataFetchingOne/> */}
+      <DataFetchingTwo/>
+    </>
+  );
+}
+
+export default App;
