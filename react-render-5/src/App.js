64dd5e32a436e6f4bdfaf6fbd1876f6a5e204150
AdamStajer07 <stajeradam9@gmail.com>
2023-12-29 22:16:07 +0100

    React Render Lesson about Sam Element Reference

---
+++ react-render-5/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ChildOne } from './components/ChildOne';
+import { GrandParent } from './components/GrandParent';
+import { ParentOne } from './components/ParentOne';
+
+function App() {
+  return (
+    <>
+      <GrandParent/>
+    </>
+  );
+}
+
+export default App;
