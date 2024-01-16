a83ebc6c91da3e7dafef16086b5e59e92af885db
AdamStajer07 <stajeradam9@gmail.com>
2024-01-16 21:02:14 +0100

    lesson about context in React

--- fifth-part/src/App.js
+++ fifth-part/src/App.js
@@ -1,9 +1,11 @@
 import ClickCounter from "./components/ClickCounter";
 import ClickCounter2 from "./components/ClickCounter2";
+import ComponentA from "./components/ComponentA";
 import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
 import HoverCounter2 from "./components/HoverCounter2";
 import User from "./components/User";
+import { UserProvider } from "./components/UserContext";
 
 function App() {
   return (
@@ -11,11 +13,15 @@ function App() {
       {/* <ClickCounter name="Adam"/> */}
       {/* <HoverCounter/> */}
 
-      <Counter render={(count, incrementCount)=><ClickCounter2 count={count} incrementCount={incrementCount}/>}/>
-      <Counter render={(count, incrementCount)=><HoverCounter2 count={count} incrementCount={incrementCount}/>}/>
+      {/* <Counter render={(count, incrementCount)=><ClickCounter2 count={count} incrementCount={incrementCount}/>}/> */}
+      {/* <Counter render={(count, incrementCount)=><HoverCounter2 count={count} incrementCount={incrementCount}/>}/> */}
       {/* <ClickCounter2/> */}
       {/* <HoverCounter2/> */}
-      <User render={(isLoggedIn)=>isLoggedIn?"Adam":"Guest"}/>
+      {/* <User render={(isLoggedIn)=>isLoggedIn?"Adam":"Guest"}/> */}
+
+      <UserProvider value="Adam">
+        <ComponentA/>
+      </UserProvider>
     </>
   );
 }
