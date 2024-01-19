4a802c817c67489fbb6c1ab2620c88eebff7702c
AdamStajer07 <stajeradam9@gmail.com>
2024-01-19 19:39:19 +0100

    lessons about HTTP Request in React

---
+++ sixth-part/src/App.js
@@ -0,0 +1,13 @@
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <div>
+      {/* <PostList/> */}
+      <PostForm/>
+    </div>
+  );
+}
+
+export default App;
