0fc1b42b36503834e4a1bac7d37f5b9fa70e9ef2
AdamStajer07 <stajeradam9@gmail.com>
2024-01-29 17:07:17 +0100

    4/15 practice projects in react from FreeCodeCamp. This projectis accordion (FAQ)

---
+++ project-accordion/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [question, setQuestion] = useState(data)
+  return <main>
+    <div className='container'>
+      <h3>Questions and Answers about login</h3>
+      <section className='info'>
+        {question.map(question=>{
+          return <SingleQuestion key={question.id} {...question}/>
+        })}
+      </section>
+    </div>
+  </main>
+}
+
+export default App;
