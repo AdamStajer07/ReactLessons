33b0477a1888bd920350a1aa4cbdf1c4777853e5
AdamStajer07 <stajeradam9@gmail.com>
2024-02-21 21:36:10 +0100

    Project timer with use useRef

---
+++ project-timer/src/App.js
@@ -0,0 +1,62 @@
+import { useState, useRef } from 'react'
+
+function App() {
+  const [randomInput, setRandomInput] = useState('')
+  const [seconds, setSeconds] = useState(0)
+  
+  const renders = useRef(0)
+  const timerId = useRef()
+
+  const handleChange = (e) => {
+    setRandomInput(e.target.value)
+    renders.current++
+  }
+
+  const startTimer = () => {
+    timerId.current = setInterval(()=>{
+      renders.current++
+      setSeconds(prev => prev+1)
+    }, 1000)
+  }
+
+  const stopTimer = () => {
+    clearInterval(timerId.current)
+    timerId.current = 0
+  }
+
+  const resetTimer = () => {
+    stopTimer()
+    if(seconds) {
+      renders.current++
+      setSeconds(0)
+    }
+  }
+  
+  return (
+    <main className="App">
+      <input
+        type="text"
+        value={randomInput}
+        placeholder="Random Input"
+        onChange={handleChange}
+      />
+      <p>Renders: {renders.current}</p>
+      <br /><br />
+      <section>
+        <button onClick={startTimer}>Start</button>
+      </section>
+      <section>
+        <button onClick={stopTimer}>Stop</button>
+      </section>
+      <section>
+        <button onClick={resetTimer}>Reset</button>
+      </section>
+      <br /><br />
+      <p>seconds: {seconds}</p>
+      <br /><br />
+      <p>{randomInput}</p>
+    </main>
+  )
+}
+
+export default App
