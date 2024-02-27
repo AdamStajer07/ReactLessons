c19896d25871f00be7d9b726699d249ef5fb1333
AdamStajer07 <stajeradam9@gmail.com>
2024-02-27 20:19:38 +0100

    12/15 practice projects in react from FreeCodeCamp. This project is sidebar

---
+++ project-sidebar/src/Home.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
+const Home = () => {
+  const {openModal, openSidebar} = useGlobalContext()
+  return <main>
+    <button className='sidebar-toggle' onClick={openSidebar}><FaBars/></button>
+    <button className='btn' onClick={openModal}>Show modal</button>
+  </main>
+}
+
+export default Home
