0f37c73d5194bb18b05206c6b175a1c287cb4dc3
AdamStajer07 <stajeradam9@gmail.com>
2024-03-07 21:01:31 +0100

    15/15 practice projects in react from FreeCodeCamp. This project is coctail-shop side with react-router

---
+++ project-coctails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Here should be side's description
+      </p>
+    </section>
+  )
+}
+
+export default About
