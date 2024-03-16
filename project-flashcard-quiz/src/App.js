9eee3bada9c9ceb25670067bec2878bec388de48
AdamStajer07 <stajeradam9@gmail.com>
2024-03-16 18:48:14 +0100

    Flashcard quiz project from Web Dev Simpled

---
+++ project-flashcard-quiz/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import FlashcardList from "./FlashcardList";
+import './App.css';
+
+function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  return (
+    <>
+      <FlashcardList flashcards={flashcards} />
+    </>
+  );
+}
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
+
+export default App;
