bcfd49e3609c843cc76d77913db36eeab648b20e
AdamStajer07 <stajeradam9@gmail.com>
2024-04-03 21:14:58 +0200

    React Redux Tutorial from CodeEvolution. Hooks in Redux

--- redux-2/src/App.js
+++ redux-2/src/App.js
@@ -1,10 +1,12 @@
 import { Provider } from 'react-redux';
-import { store } from './redux/store'
+import store from './redux/store'
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
+      <HooksCakeContainer />
       <CakeContainer />
     </Provider>
   );
