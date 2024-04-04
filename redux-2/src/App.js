ebb258e4886972df0cc2507b08ec5441002ae396
AdamStajer07 <stajeradam9@gmail.com>
2024-04-04 20:21:21 +0200

    React Redux Tutorial from CodeEvolution. Multi reducer

--- redux-2/src/App.js
+++ redux-2/src/App.js
@@ -2,12 +2,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <HooksCakeContainer />
-      <CakeContainer />
+      <IceCreamContainer />
+      {/* <CakeContainer /> */}
     </Provider>
   );
 }
