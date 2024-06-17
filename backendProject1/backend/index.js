e7503e680f65aa37096a596fd6b1080b8964e310
AdamStajer07 <stajeradam9@gmail.com>
2024-06-17 13:16:46 +0200

    Backend Project from Lama dev. Project library use React, Express and MySQL

--- backendProject1/backend/index.js
+++ backendProject1/backend/index.js
@@ -30,17 +30,37 @@ app.get("/books", (req, res) => {
 });
 
 app.post('/books', (req, res) => {
-    const q = 'INSERT INTO books (`title`, `desc`, ``cover) VALUES (?)'
-    const values = [req.body.title, req.body.desc, req.body.cover]
+    const q = 'INSERT INTO books (`title`, `desc`, `cover`, `price`) VALUES (?)'
+    const values = [req.body.title, req.body.desc, req.body.cover, req.body.price]
 
     db.query(q, [values], (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
+        if (err) return res.json(err);
         return res.json('Book has been created succesfully');
     })
 })
+
+app.delete('/books/:id', (req, res) => {
+    const bookId = req.params.id
+    const q = 'DELETE FROM books WHERE id = ?'
+
+    db.query(q, [bookId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json('Book has been deleted succesfully');
+    })
+})
+
+app.put('/books/:id', (req, res) => {
+    const bookId = req.params.id
+    const values = [req.body.title, req.body.desc, req.body.cover, req.body.price]
+
+    const q = 'UPDATE books SET `title`= ?, `desc` = ?, `cover` = ?, `price` = ? WHERE id = ?'
+
+    db.query(q, [...values, bookId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json('Book has been updated succesfully');
+    })
+})
+
 
 app.listen(8800, () => {
     console.log('Connected to backend')
--- backendProject1/client/src/pages/Books.jsx
+++ backendProject1/client/src/pages/Books.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import 'axios'
 import axios from 'axios'
+import {Link, useNavigate} from 'react-router-dom'
 
 export const Books = () => {
     const [books, setBooks] = useState([])
+    const navigate = useNavigate()
 
     useEffect(()=>{
         const fetchAllBooks = async ()=> {
@@ -18,19 +19,35 @@ export const Books = () => {
         fetchAllBooks()
     },[])
     
+    const handleDelete = async id => {
+        try {
+            await axios.delete('http://localhost:8800/books/'+id)
+            window.location.reload()
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+    
   return (
-    <div>
-        <h1>Books shop</h1>
+    <>
+        <h1 className='title'>Books shop</h1>
         <div className="books">
             {books.map(el => { 
-                const {id, title, desc, cover} = el
-                return <div key={id}>
-                    <p>{id}</p>
-                    <p>{title}</p>
-                    <p>{desc}</p>
+                const {id, title, desc, price, cover} = el
+                return <div key={id} className='book'>
+                    <img src={`../${cover}.jpg`} alt={cover} />
+                    <h1 className='book__title'>{title}</h1>
+                    <p className='book__desc'>{desc}</p>
+                    <p className='book__price'>{price}zł</p>
+                    <div>
+                        <button className="update"><Link to={`/update/${id}`}>Update</Link></button>
+                        <button className="delete" onClick={()=>handleDelete(id)}>Delete</button>
+                    </div>
                 </div>
             })}
         </div>
-    </div>
+        <button onClick={()=>navigate('/add')}>Add books</button>
+    </>
   )
 }
