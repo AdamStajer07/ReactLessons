daa1a02f24bde174fc4737397cf740f9f3a1a70a
AdamStajer07 <stajeradam9@gmail.com>
2024-09-27 20:20:58 +0200

    Add SignIn and SignUp commend

--- notebook/back-end/index.js
+++ notebook/back-end/index.js
@@ -4,15 +4,49 @@ import cors from 'cors'
 
 const app = express()
 
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "notebook"
-// })
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "notebook"
+})
 
 app.use(express.json())
 app.use(cors())
+
+app.get('/signIn', (req, res) => {
+    const q = `SELECT COUNT(user_id) FROM users WHERE email='${req.body.email}' AND password='${req.body.password}'`
+    db.query(q, (err, data) => {
+        if (err) {
+            console.log(err)
+            return res.json(err)
+        }
+        if (data == 1) return true
+        else return false
+    })
+})
+
+app.post('/signUp', (req, res) => {
+    const q = `INSERT INTO users VALUES(NULL, '${req.body.email}', '${req.body.password}')`
+    db.query(q, (err, data) => {
+        if (err) {
+            console.log(err)
+            return res.json(err)
+        }
+        else return res.json('Success')
+    })
+})
+
+app.get('/', (req, res) => {
+    const q = "SELECT * FROM users";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.log(err)
+            return res.json(err)
+        }
+        return res.json(data)
+    })
+})
 
 app.listen(8800, () => {
     console.log('Connected to backend')
